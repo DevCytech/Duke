2111ba1901fde5fc2103c31915bd2fb2c0311e83
Cytech <22370499+CytechYT@users.noreply.github.com>
2021-02-12 15:43:12 -0500

    Started work on updating models
    Updated the guild model

---
+++ src/models/guild.js
@@ -0,0 +1,69 @@
+const { Schema, model } = require('mongoose');
+const { config } = require('../tools');
+
+const guildSchema = new Schema({
+	guildID: String,
+	guildName: String,
+	guildOwnerID: String,
+	createdDate: String,
+	guildMembers: Number,
+	// Leveling
+	xpCoolDown: { type: Number, default: 30000 },
+	// Suggestions
+	suggestionCount: { type: Number, default: 0 },
+	// Settings
+	prefix: { type: String, default: config.prefix },
+	// Logging
+	logChannel: String.fromCharCode,
+	reportChannel: String,
+	// Moderation
+	mutedRole: String,
+	// Disabled
+	disabled: { type: Boolean, default: [] },
+});
+
+module.exports.model = model('guilds', guildSchema);
+module.exports.getGuild = async (guild) => {
+	switch (typeof guild) {
+		case 'string': {
+			const guildData = await this.model
+				.findOne({ guildID: guild })
+				.catch((err) => {
+					console.error(err);
+				});
+
+			return guildData;
+		}
+
+		case 'object': {
+			const guildData = await this.model
+				.findOne({ guildID: guild.id })
+				.catch((err) => {
+					console.error(err);
+				});
+
+			// If there is no guild data
+			if (!guildData) {
+				const newGuild = new this.model({
+					guildID: guild.id,
+					guildName: guild.name,
+					guildOwnerID: guild.ownerId,
+					createdDate: guild.createdAt,
+					guildMembers: guild.memberCount,
+				});
+				return await newGuild.save();
+			}
+
+			// If the guild name is out of date
+			if (guildData.name !== guild.name) {
+				guildData.name = guild.name;
+				await guildData.save();
+			}
+
+			return guildData;
+		}
+
+		default:
+			throw new Error('Unsupported guild type.');
+	}
+};
