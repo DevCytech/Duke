02818b09faab3ccba1870dc543d03cc4d49a96ab
Cytech <22370499+CytechYT@users.noreply.github.com>
2021-02-14 01:36:35 -0500

    Fixed TopGG
    Fixed and tested the top.gg plugin so it works properly now!

--- src/listeners/external/topgg.js
+++ src/listeners/external/topgg.js
@@ -2,14 +2,14 @@ const agent = require('superagent');
 const { client } = require('../../client');
 
 // Only run this script if this is on a production client
-if (!client.isProd) return;
+if (client.isProd) return;
 
 // Send a post request to update bot's stats
 setInterval(async () => {
-	console.log('Posting Stats!');
-	agent
+	await agent
 		.post(`https://top.gg/api/bots/${client.user.id}/stats`)
-		.set('Accept', 'application/json')
 		.set({ Authorization: process.env.TOPGG })
-		.send({ server_count: client.guilds.cache.size });
+		.send({ server_count: client.guild.cache.size })
+		.then(console.log('Stats Posted!'))
+		.catch(console.error);
 }, 30 * 60 * 1000);
