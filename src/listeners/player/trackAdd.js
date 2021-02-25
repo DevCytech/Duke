b74eadcbc6abd1e018bd8be0f7c3728875acff8d
Cytech <22370499+DevCytech@users.noreply.github.com>
2021-02-25 09:15:55 -0500

    Testing music player events

---
+++ src/listeners/player/trackAdd.js
@@ -0,0 +1,5 @@
+const { client } = require('../../client');
+
+client.player.on('trackAdd', (message, queue, track) => {
+  return message.channel.send(`🎵 ${track.title} has been added to the queue!`);
+})
